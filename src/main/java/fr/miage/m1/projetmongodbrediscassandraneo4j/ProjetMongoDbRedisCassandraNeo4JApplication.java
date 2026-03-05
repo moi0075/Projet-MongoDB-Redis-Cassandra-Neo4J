effd7a62b208d7492f2bad5dc1c26619c12fe0d1
moi0075 <teoviglietti05@gmail.com>
2026-03-05 14:34:41 +0100

    First commit 2

---
+++ src/main/java/fr/miage/m1/projetmongodbrediscassandraneo4j/ProjetMongoDbRedisCassandraNeo4JApplication.java
@@ -0,0 +1,13 @@
+package fr.miage.m1.projetmongodbrediscassandraneo4j;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetMongoDbRedisCassandraNeo4JApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProjetMongoDbRedisCassandraNeo4JApplication.class, args);
+    }
+
+}
---
+++ src/test/java/fr/miage/m1/projetmongodbrediscassandraneo4j/ProjetMongoDbRedisCassandraNeo4JApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.miage.m1.projetmongodbrediscassandraneo4j;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetMongoDbRedisCassandraNeo4JApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
